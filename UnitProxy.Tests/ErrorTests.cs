47ff2e2d26f9edb0cd4dd221e8b8d8d1ce359ce4
Jesper Larsen-Ledet <jll@trackman.com>
2018-10-04 12:53:17 +0200

    More failure cases

---
+++ UnitProxy.Tests/ErrorTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityProxy;
+using Xunit;
+
+namespace UnitProxy.Tests
+{
+    public class ErrorTests
+    {
+        [Theory]
+        [InlineData(true,
+            "Assets/Perfect Parallel/Perfect Engine External/Scripts/CommandLine.cs(88,63): error CS0103: The name `WebUtility' does not exist in the current context")]
+        public void IsError(bool error, string line)
+        {
+            var isError = ErrorFinder.IsError(line);
+            Assert.Equal(error, isError);
+        }
+    }
+}
--- UnityProxy/Watcher.cs
+++ UnityProxy/Watcher.cs
@@ -1,129 +1,142 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace UnityProxy
 {
-	/// <summary>
-	/// Watches the Unity log file and redirects it to standard output.
-	/// </summary>
-	public class Watcher
-	{
-		/// <summary>
-		/// Magic string for detecting progress bar messages.
-		/// </summary>
-		private const string ProgressBarMarker = "DisplayProgressbar: ";
+    /// <summary>
+    /// Watches the Unity log file and redirects it to standard output.
+    /// </summary>
+    public class Watcher
+    {
+        /// <summary>
+        /// Magic string for detecting progress bar messages.
+        /// </summary>
+        private const string ProgressBarMarker = "DisplayProgressbar: ";
 
-		/// <summary>
-		/// Size of the log when it was previously read.
-		/// </summary>
-		private static long previousLogSize = 0;
+        /// <summary>
+        /// Size of the log when it was previously read.
+        /// </summary>
+        private static long previousLogSize = 0;
 
-		/// <summary>
-		/// Indicates if this thread should stop.
-		/// </summary>
-		private volatile bool shouldStop = false;
+        /// <summary>
+        /// Indicates if this thread should stop.
+        /// </summary>
+        private volatile bool shouldStop = false;
 
-		/// <summary>
-		/// Full log text.
-		/// </summary>
-		private volatile string fullLog = "";
+        /// <summary>
+        /// Full log text.
+        /// </summary>
+        private volatile string fullLog = "";
 
-		/// <summary>
-		/// Path to the log file.
-		/// </summary>
-		private string logPath;
+        /// <summary>
+        /// Path to the log file.
+        /// </summary>
+        private string logPath;
 
 
-		/// <summary>
-		/// Gets the full log text.
-		/// </summary>
-		public string FullLog
-		{
-			get
-			{
-				return fullLog;
-			}
-		}
+        /// <summary>
+        /// Gets the full log text.
+        /// </summary>
+        public string FullLog
+        {
+            get
+            {
+                return fullLog;
+            }
+        }
 
-		/// <summary>
-		/// Creates a new Watcher that will read the log from the specified path.
-		/// </summary>
-		/// <param name="logPath"></param>
-		public Watcher(string logPath)
-		{
-			this.logPath = logPath;
-		}
+        /// <summary>
+        /// Creates a new Watcher that will read the log from the specified path.
+        /// </summary>
+        /// <param name="logPath"></param>
+        public Watcher(string logPath)
+        {
+            this.logPath = logPath;
+        }
 
-		private const string FailureString = "Build failure!";
-		private const string ErrorString = "ERROR:";
-		public bool Failed;
-		public void Run()
-		{
-			while (true)
-			{
-				if (File.Exists(logPath))
-				{
-					using (FileStream stream = new FileStream(logPath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
-					{
-						stream.Position = previousLogSize;
-						previousLogSize = stream.Length;
+        public bool Failed;
+        public void Run()
+        {
+            while (true)
+            {
+                if (File.Exists(logPath))
+                {
+                    using (FileStream stream = new FileStream(logPath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+                    {
+                        stream.Position = previousLogSize;
+                        previousLogSize = stream.Length;
 
-						using (StreamReader reader = new StreamReader(stream))
-						{
-							string newText = reader.ReadToEnd();
-							LogProgressMessages(newText);
-							fullLog += newText;
-							Console.Write(newText);
-							if (!Failed)
-							{
-								var lines = newText.Split('\r', '\n');
+                        using (StreamReader reader = new StreamReader(stream))
+                        {
+                            string newText = reader.ReadToEnd();
+                            LogProgressMessages(newText);
+                            fullLog += newText;
+                            Console.Write(newText);
+                            if (!Failed)
+                            {
+                                var lines = newText.Split('\r', '\n');
 
-								var failures = lines.Where(x => x.Contains(FailureString)).
-									Select(x => x.Substring(x.IndexOf(FailureString, StringComparison.Ordinal) + FailureString.Length).Trim());
+                                foreach (var line in lines)
+                                {
+                                    if (ErrorFinder.IsError(line))
+                                    {
+                                        Failed = true;
+                                        Console.WriteLine($"##teamcity[buildProblem description='{line.Replace("'", "\"")}']");
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
 
-								var errors = lines.Where(x => x.Contains(ErrorString)).
-									Select(x => x.Substring(x.IndexOf(ErrorString, StringComparison.Ordinal) + ErrorString.Length).Trim());
+                if (shouldStop) break;
 
-								var failure = failures.Concat(errors).FirstOrDefault();
-								if (failure != null)
-								{
-									Failed = true;
-									Console.WriteLine($"##teamcity[buildProblem description='{failure.Replace("'","\"")}']");
-								}
-							}
-						}
-					}
-				}
+                System.Threading.Thread.Sleep(1000);
+            }
+        }
 
-				if (shouldStop) break;
+        /// <summary>
+        /// Searches for progress bar messages and forwards them to TeamCity.
+        /// </summary>
+        /// <param name="text"></param>
+        private void LogProgressMessages(string text)
+        {
+            string[] lines = text.Split(Environment.NewLine.ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 0; i < lines.Length; i++)
+            {
+                if (lines[i].StartsWith(ProgressBarMarker))
+                {
+                    string progressName = lines[i].Substring(ProgressBarMarker.Length);
+                    Console.WriteLine();
+                    Console.WriteLine("##teamcity[progressMessage '" + progressName + "']");
+                }
+            }
+        }
 
-				System.Threading.Thread.Sleep(1000);
-			}
-		}
+        //Stops the watcher.
+        public void Stop()
+        {
+            shouldStop = true;
+        }
+    }
 
-		/// <summary>
-		/// Searches for progress bar messages and forwards them to TeamCity.
-		/// </summary>
-		/// <param name="text"></param>
-		private void LogProgressMessages(string text)
-		{
-			string[] lines = text.Split(Environment.NewLine.ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
-			for (int i = 0; i < lines.Length; i++)
-			{
-				if (lines[i].StartsWith(ProgressBarMarker))
-				{
-					string progressName = lines[i].Substring(ProgressBarMarker.Length);
-					Console.WriteLine();
-					Console.WriteLine("##teamcity[progressMessage '" + progressName + "']");
-				}
-			}
-		}
+    public static class ErrorFinder
+    {
+        public static bool IsError(string line)
+        {
+            foreach (var pattern in new[] { "Exception: ", "Build failure!", "Compilation failed", "ERROR:", "): error CS", "Error building Player" })
+            {
+                if (line.Contains(pattern))
+                {
+                    return true;
+                }
+            }
 
-		//Stops the watcher.
-		public void Stop()
-		{
-			shouldStop = true;
-		}
-	}
+            return false;
+
+        }
+    }
 }
